8d3a385b0b4b7058135b24154f16ca1030903701
York Zhao <zys0307@outlook.com>
2022-04-18 19:26:43 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-/usr/bin/v2ray --config https://gist.github.com/York618/a30afb635c596e2e8fcb9e98f5d31da9/raw/9aace8967b6b2a32a2678be08f9df95ff8fbbf41/v2ray-config
+/usr/bin/v2ray --config https://gist.github.com/York618/a30afb635c596e2e8fcb9e98f5d31da9/raw/400191826c0b68aa549b58479f54b9272b7a33c3/v2ray-config
